ead54c39213a7c9c413c0adf7dc8e90d8e27ad0c
akshatt <akshat.trigunait@group.one>
2025-11-18 13:27:18 +0530

    Add `ProductDetailRankMath` component to display and compare plugin details with pro version

--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3beb35c8e3576aff76f6');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '93fc8c0302c05e6a1a9d');
