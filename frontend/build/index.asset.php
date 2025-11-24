84d1828de43f699ec9bce821929d8f9a21724813
akshatt <akshat.trigunait@group.one>
2025-11-24 17:18:08 +0530

    Update plugin marketplace: enhance UI, improve price logic, add manage button, and refine text handling

--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '70412354d2f9451230e2');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '90f1ffa36b5b667d67b1');
