ca44ccbac998999ea139f5cb8c1fe176291dc407
akshatt <akshat.trigunait@group.one>
2025-12-12 17:04:13 +0530

    Add Mixpanel tracking for "Get Started" button in SuccessNotice component

--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'c906e416f5abbc095812');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '530445649b0b1e558e2f');
