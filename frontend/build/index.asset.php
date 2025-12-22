ec29fb9d255b468ca9dc17aa59bbcaabe64e4ef2
akshatt <akshat.trigunait@group.one>
2025-12-22 17:06:55 +0530

    Add dynamic free trial label support in `MarketPlace` and `FeaturedCarousel` components

--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '18b4ac5bf7fd13803838');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd7965b53292c5dbd2d1c');
