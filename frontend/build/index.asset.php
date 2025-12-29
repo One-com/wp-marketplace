e3013111158eede43cc6f1d98e3c7b4b983d051f
akshatt <akshat.trigunait@group.one>
2025-12-29 12:32:45 +0530

    Refactor WP version error state component to simplify logic and improve i18n consistency.

--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '949639a8cf6f71920af8');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'af9fdff77213c463d522');
