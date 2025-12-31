2871167787e697a6c98b5066b9c09faa175f68c8
Prateek Jain <prateekj@one.com>
2025-12-31 16:45:14 +0530

    randoms

--- frontend/build/addons.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '7054c82b53103ab8bce2');
--- frontend/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3624204fc74bb8e633a0');
