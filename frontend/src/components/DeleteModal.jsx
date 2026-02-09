a4162fe130c8c68718bfd1847c39f828a080a353
dineshkashera <dineshk@one.com>
2026-02-09 17:27:12 +0530

    Handle escape, outside click of delete modal

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {formatMessage, replacePercentWrapper, HtmlRenderer} from '../utils/common.utils';
 const DeleteModal = ({ isOpen, onClose, onConfirm, pluginName, uiI18n, iconBase, licenseType }) => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = '';
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
+
+  const handleOutsideClick = (e) => {
+    if (e.target.classList.contains('gv-modal')) {
+      onClose();
+    }
+  };
+
   const isPremium = licenseType !== "free";
   return (
-    <div className="gv-modal">
+    <div className="gv-modal" onClick={handleOutsideClick}>
       <div
         className="gv-modal-content"
         role="dialog"
