34a3d101650999a6b9fa6853bbcd5e26da42d93f
dineshkashera <dineshk@one.com>
2026-03-19 19:10:16 +0530

    Subscription list in-progress

--- backend/src/Controllers/MarketplaceController.php
+++ backend/src/Controllers/MarketplaceController.php
@@ -173,6 +173,8 @@ class MarketplaceController {
 			add_action( 'wp_ajax_marketplace_clear_pending_procurement', [ $this, 'ajax_clear_pending_procurement' ] );
 			add_action( 'wp_ajax_marketplace_subscribe', [ $this, 'ajax_subscribe' ] );
 
+			add_action( 'wp_ajax_marketplace_get_subscriptions_list', [ $this, 'get_subscriptions_list' ] );
+
 			//reset transient for marketplace catalog
 			add_action('upgrader_process_complete', [$this, 'reset_transient_on_core_update'], 10, 2);
 			add_action('update_option_WPLANG', [$this, 'reset_transient_on_locale_change'], 999, 0);
@@ -1122,6 +1124,36 @@ class MarketplaceController {
 		);
 
 		$result = $this->get_model()->request( $payload, 'POST' );
+
+		if ( is_wp_error( $result ) ) {
+			wp_send_json_error( [ 'message' => $result->get_error_message() ] );
+		}
+
+		if ( isset( $result['error'] ) && $result['error'] ) {
+			wp_send_json_error( $result );
+		}
+
+		wp_send_json_success( $result );
+	}
+
+	/**
+	 *
+	 * @return void
+	 */
+	public function get_subscriptions_list() {
+
+		check_ajax_referer( 'marketplace_nonce', 'nonce' );
+
+		$payload = array_merge(
+			$this->config['payload'] ?? [],
+			[
+				'action' => 'wp-marketplace-subscription-list',
+				'username' => 'test-6202', //TODO: remove this after testing
+				'api_key'  => 'd05fbebee53d13108a3c96747901f58f', //TODO: remove this after testing
+			]
+		);
+
+		$result = $this->get_model()->request( $payload, 'GET' );
 
 		if ( is_wp_error( $result ) ) {
 			wp_send_json_error( [ 'message' => $result->get_error_message() ] );
