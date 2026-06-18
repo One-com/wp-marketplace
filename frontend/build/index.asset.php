3fcbe1e7feab713be5638cdfb29aaa04596e8529
Dinesh K <dinesh.kashera@group.one>
2026-06-18 17:37:04 +0530

    Build

--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'faf0f5809a3017d78cb4');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'c82a55342ddf4f81ad06');
