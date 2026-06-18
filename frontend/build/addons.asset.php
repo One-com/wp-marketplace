204b86a6ae800b550c6c5ac0ef37baef167e8936
Dinesh K <dinesh.kashera@group.one>
2026-06-18 13:57:42 +0530

    Build after add sale tag and retail price

--- frontend/build/addons.asset.php
+++ frontend/build/addons.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '71be2bc53c751e47c53f');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'bd2cef5b2ab829ccc5e9');
--- frontend/build/index.asset.php
+++ frontend/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'a185fb8a472b7f8584a7');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'a6c7bf0352b295ae79f2');
